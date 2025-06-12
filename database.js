d639f475fa9f50788c5a3ac5bbe90d40f382085f
Dell muốn làm <135613676+Lazydoz@users.noreply.github.com>
2025-06-12 15:58:52 +0000

    them bang cloth vao database

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-/* KET NOI TOI CO SO DU LIEU */ 
+/*----- KET NOI TOI CO SO DU LIEU -----*/ 
 const db = new sqlite3.Database('./data/lzdb.db', (err) => {
 
     if (err) {
@@ -28,29 +28,44 @@ const insert = (booth_id, base_name, image, source, download) => {
 }
 
 db.serialize(() => {
-    /* TAO BANG */
+    
+    /*----- TAO BANG -----*/
+
+    // BANG BASE
     db.run(`
-    CREATE TABLE IF NOT EXISTS base (
-        base_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        booth_id INTEGER,
-        base_name TEXT,
-        image TEXT,
-        source TEXT,
-        download TEXT
-    )
+        CREATE TABLE IF NOT EXISTS base (
+            base_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            booth_id INTEGER,
+            base_name TEXT,
+            image TEXT,
+            source TEXT,
+            download TEXT
+        )
     `);
 
-//    insert(6214004,"Skimmia","https://booth.pximg.net/af3e0f0e-13f8-436b-b90b-e3a2ed57ca12/i/6214004/dd3cee5c-c52b-4bf4-b2ef-7fa3c15fae47_base_resized.jpg","https://booth.pm/ja/items/6214004","https://drive.google.com/file/d/1Z4q49o_NgT7T6xHmAFfQuBYLD0I9CEwr/view?usp=sharing");
+    // BANG CLOTH
+    db.run(`
+        CREATE TABLE IF NOT EXISTS cloth (
+            cloth_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            booth_id INTEGER,
+            cloth_name TEXT,
+            image TEXT,
+            source TEXT,
+            download TEXT
+        )
+    `);
+
+    //insert(6214004,"Skimmia","https://booth.pximg.net/af3e0f0e-13f8-436b-b90b-e3a2ed57ca12/i/6214004/dd3cee5c-c52b-4bf4-b2ef-7fa3c15fae47_base_resized.jpg","https://booth.pm/ja/items/6214004","https://drive.google.com/file/d/1Z4q49o_NgT7T6xHmAFfQuBYLD0I9CEwr/view?usp=sharing");
 
     // XEM KIEU JSON
-    db.all("SELECT * FROM base", [], (error, rows) => {
+    db.all("SELECT * FROM base", [], (err, rows) => {
         console.table(rows);
     });
     
 
 });
 
-/* DONG KET NOI TOI CO SO DU LIEU */
+/*----- DONG KET NOI TOI CO SO DU LIEU -----*/
 db.close((err) => {
 
     if (err) {
