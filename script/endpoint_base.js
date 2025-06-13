3858c51942eab9e73010e8979dc626bfd90e17ad
Dell muốn làm <135613676+Lazydoz@users.noreply.github.com>
2025-06-13 09:31:25 +0000

    rieng endpoint  cho base

---
+++ script/endpoint_base.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./data/shop.db');
+
+// tao bo dinh tuyen
+const router = express.Router();
+
+
+// lay tat ca base avatar
+router.get('/', (req,res) => {
+    db.all("SELECT * FROM base", [], (err,rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        else {
+            res.json(rows);
+        }
+    });
+});
+
+module.exports = router;
