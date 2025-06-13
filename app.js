d37ca108b16b40890b665f1f2b1907bcea40daa2
Dell muốn làm <135613676+Lazydoz@users.noreply.github.com>
2025-06-13 18:49:25 +0000

    hoan thien chuc nang them xoa sua

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const experss = require('express');
+const express = require('express');
 const path = require('path');
 const open = require('open').default;
 
 
 const endpoint_base = require('./script/endpoint_base.js');
 
-const app = experss();
+const app = express();
 
-app.use(experss);
+app.use(express);
 app.use('/api/base',endpoint_base);
 
 
 // xu ly cac thu muc tinh
-app.use(experss.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname)));
 
 // tra ve index.html
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-  open('http://localhost:3000');
+app.listen(8000, () => {
+  console.log('Server running on http://localhost:8000/');
+  open('http://localhost:8000/');
 });
--- script/endpoint_base.js
+++ script/endpoint_base.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./data/shop.db');
+const db = new sqlite3.Database('./data/lzdb.db');
 
 // tao bo dinh tuyen
 const router = express.Router();
